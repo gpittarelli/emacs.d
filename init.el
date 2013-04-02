c613d15ba4959733d6d1c849ac3fbb58f4d0f40e
George Pittarelli <george@gpittarelli.com>
2013-04-02 13:25:39 +0000

    Add cmake-mode, eval-and-replace at C-x C-e

--- init.el
+++ init.el
@@ -204,6 +204,19 @@
 ;; allow scroll-down/up-command to move point to buffer end/beginning
 (setq scroll-error-top-bottom 'true)
 
+(require 'cmake-mode)
+
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+(global-set-key (kbd "C-x C-e") 'eval-and-replace)
+
 ;; setting the PC keyboard's various keys to
 ;; Super or Hyper, for emacs running on Windows.
 (setq w32-pass-lwindow-to-system nil
