939474dbec4a45aadde8e0921b95e5423781c2b1
George Pittarelli <g.pittarelli@gmail.com>
2013-05-11 00:53:38 -0400

    Make it harder to quit emacs

--- key-bindings.el
+++ key-bindings.el
@@ -1,3 +1,8 @@
+;; Why leave emacs with something as easy as C-x C-c?
+;; instead, the mnemonic is C-x REALLY QUIT
+(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-x C-c") 'delete-frame)
+
 (global-set-key (kbd "C-<f1>") 'show-file-name)
 
 ;; Unbind Pesky Sleep Button
